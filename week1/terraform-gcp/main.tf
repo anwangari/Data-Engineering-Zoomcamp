90f7e5c19ff489819866a255da5acf7123cae497
Anthony Njuguna <98034679+anwangari@users.noreply.github.com>
2024-02-20 09:03:57 +0000

    updated main.tf file up to configuration options

--- week1/terraform-gcp/main.tf
+++ week1/terraform-gcp/main.tf
@@ -1,20 +1,14 @@
-
-# to be edited
-resource "google_storage_bucket" "static-site" {
-  name          = "image-store.com"
-  location      = "EU"
-  force_destroy = true
-
-  uniform_bucket_level_access = true
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.16.0"
+    }
   }
-  cors {
-    origin          = ["http://image-store.com"]
-    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-    response_header = ["*"]
-    max_age_seconds = 3600
-  }
+}
+
+provider "google" {
+  # Configuration options
+  project     = "thematic-mapper-412506"
+  region      = "us-central1"
 }
